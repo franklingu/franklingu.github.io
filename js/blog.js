78fe51f4b6fe01e4bf40f4c2f6d5f467dd2e376e
franklingu <franklingujunchao@gmail.com>
2014-11-25 00:09:09 +0800

    finished front page styling

---
+++ js/blog.js
@@ -0,0 +1,5 @@
+$(function () {
+  var pageTitle = $(document).find('title').text();
+  var elem = $('.navbar .navbar-nav li:contains(' + pageTitle.substr(0, 4) + ')').addClass('active');
+  console.log(elem);
+});
