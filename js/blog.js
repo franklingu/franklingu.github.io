bdff7fcf22d70d86ae28662f1c284bdca8857ebf
franklingu <franklingujunchao@gmail.com>
2014-11-25 13:28:07 +0800

    add a simple search

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,21 @@
 $(function () {
   var pageTitle = $(document).find('title').text();
-  var elem = $('.navbar .navbar-nav li:contains(' + pageTitle.substr(0, 4) + ')').addClass('active');
+  $('.navbar .navbar-nav li:contains(' + pageTitle.substr(0, 4) + ')').addClass('active');
+});
+
+$(function () {
+  $('#search-title').keyup(function () {
+    var searchText = $("#search-title").val().toLowerCase();
+    console.log(searchText);
+    $('.post-short-info').each(function (index, elem) {
+      var liElem = $(elem);
+      if (liElem.find('a').text().toLowerCase().match('^' + searchText) || searchText === "") {
+        liElem.show();
+        liElem.addClass('animated fadeIn');
+      } else {
+        liElem.removeClass('animated fadeIn');
+        liElem.hide();
+      }
+    });
+  });
 });
